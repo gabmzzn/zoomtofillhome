91df4dfc9070eaf27ee3b231caaa8e12afc934b9
Juan Gabriel Mazzoleni <51454736+gabmzzn@users.noreply.github.com>
2022-03-14 17:34:55 -0300

    improved

--- js/demo.js
+++ js/demo.js
@@ -67,20 +67,3 @@ function zoom(command) {
 }
 
 history.scrollRestoration = 'manual'
-
-// Theme switcher
-let storedTheme = localStorage.getItem('theme')
-let currentTheme = document.documentElement.getAttribute("data-theme")
-
-if (storedTheme == 'dark' || storedTheme == 'light') currentTheme = storedTheme
-else currentTheme = window.matchMedia("(prefers-color-scheme:dark)").matches ? "dark" : "light"
-
-document.documentElement.setAttribute('data-theme', currentTheme)
-
-document.getElementsByClassName("theme-toggle")[0].onclick = () => {
-    if (currentTheme == 'light') currentTheme = 'dark'
-    else currentTheme = 'light'
-
-    localStorage.setItem('theme', currentTheme)
-    document.documentElement.setAttribute('data-theme', currentTheme)
-}
