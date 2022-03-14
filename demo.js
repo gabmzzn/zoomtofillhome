99c809b89d1984359a190d5252daab8064567914
Juan Gabriel Mazzoleni <51454736+gabmzzn@users.noreply.github.com>
2022-03-14 00:19:39 -0300

    maybe?

--- demo.js
+++ demo.js
@@ -68,26 +68,6 @@ function zoom(command) {
 
 history.scrollRestoration = 'manual'
 
-
-var toggle = document.getElementsByClassName("theme-toggle")[0]
-
-var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light")
-if (storedTheme)
-    document.documentElement.setAttribute('data-theme', storedTheme)
-
-
-toggle.onclick = function () {
-    var currentTheme = document.documentElement.getAttribute("data-theme")
-    var targetTheme = "light"
-
-    if (currentTheme === "light") {
-        targetTheme = "dark"
-    }
-
-    document.documentElement.setAttribute('data-theme', targetTheme)
-    localStorage.setItem('theme', targetTheme)
-}
-
 window.onload = () => {
     document.getElementsByClassName('title')[0].style = `
         opacity: 100%;
